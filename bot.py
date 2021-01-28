d689e81c11a47c085bf8c1d364386918ebdbc548
Savas, Nathan <nathansavas@gmail.com>
2021-01-28 00:13:30 -0800

    listen and respond to bot mentions

--- bot.py
+++ bot.py
@@ -1,14 +1,39 @@
+import logging
 import tweepy
-import Tkinter as tk
+from config import create_api
 
-consumer_key = 'consumer key'
-consumer_secret = 'consumer secrets'
-access_token = 'access token'
-access_token_secret = 'access token secret'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+class MentionsListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
 
-user = api.me()
-print (user.name)
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None:
+            try:
+                self.api.update_status(
+                    status="Hi",
+                    in_reply_to_status_id=tweet.id,
+                    auto_populate_reply_metadata=True
+                )
+                logger.info("Success")
+            except Exception as e:
+                logger.error("Error on reply", exc_info=True)
+
+    def on_error(self, status):
+        logger.error(status)
+
+def main(keywords):
+    api = create_api()
+    mentions_listener = MentionsListener(api)
+    stream = tweepy.Stream(
+        auth = api.auth,
+        listener = mentions_listener
+    )
+    stream.filter(track=keywords, languages=["en"])
+
+if __name__ == "__main__":
+    main(["@photosense_bot"])
---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import tweepy
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables
+logger = logging.getLogger() # Named logger
+
+def create_api():
+    # Get credentials from .env
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    # Authenticate
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    # Validate credentials
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
