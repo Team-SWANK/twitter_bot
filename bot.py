780786e29c6fb0168519ac049ed9d61a695bd4fc
Nathan Savas <nathansavas@gmail.com>
2021-05-05 16:14:12 -0700

    Enabled all flags

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import logging, os, io, base64, requests
 import urllib.request
 import tweepy
 import numpy as np
+import piexif
 from PIL import Image
 from config import create_api
 from dotenv import load_dotenv
@@ -24,52 +25,63 @@ class MentionsListener(tweepy.StreamListener):
         logger.info(f"Processing tweet id {tweet.id}")
         is_reply = tweet.in_reply_to_status_id
 
-        # Check if tweet is a reply to another tweet containing media
-        if is_reply is not None and self.api.get_status(is_reply).entities is not None:
-            target_tweet = self.api.get_status(is_reply)
-            i = 1
-            file_names = []
-            for media in target_tweet.extended_entities['media']:
-                media_url = media['media_url']
+        flags = get_tweet_flags(tweet)
+        if "remove" not in flags:
+            # Check if tweet is a reply to another tweet containing media
+            if is_reply is not None and self.api.get_status(is_reply).entities is not None:
+                target_tweet = self.api.get_status(is_reply)
+                i = 1
+                file_names = []
+                options = "[" + ",".join(get_tweet_flags(tweet)) + "]"
+                for media in target_tweet.extended_entities['media']:
+                    media_url = media['media_url']
 
-                # Call Segmentation API for each image
-                with urllib.request.urlopen(media_url) as url:
-                    with open('temp.jpg', 'wb') as f:
-                        f.write(url.read())
-                files = {'image': open('temp.jpg', 'rb')}
-                res = requests.post(segment_url, files=files).json()
+                    # Call Segmentation API for each image
+                    with urllib.request.urlopen(media_url) as url:
+                        with open('temp.jpg', 'wb') as f:
+                            f.write(url.read())
+                    files = {'image': open('temp.jpg', 'rb')}
+                    res = requests.post(segment_url, files=files).json()
 
-                # Convert each segmentation to image file & call censoring API
-                mask_to_image(res["predictions"]).save('mask.jpg')
-                files = {'image': open('temp.jpg', 'rb'), 'mask': open('mask.jpg', 'rb')}
-                res = requests.post(censor_url, files=files)
+                    # Convert each segmentation to image file & call censoring API
+                    mask_to_image(res["predictions"]).save('mask.jpg')
+                    files = {
+                        'image': open('temp.jpg', 'rb'),
+                        'mask': open('mask.jpg', 'rb')
+                    }
+                    res = requests.post(censor_url + options, files=files)
+                    # Convert base64 response to image file
+                    if res.status_code == 200:
+                        fn = "censored" + str(i) + ".jpg"
+                        img_bytes = res.json()["ImageBytes"].encode()
+                        with open(fn, "wb") as f:
+                            f.write(base64.decodebytes(img_bytes))
+                        i += 1
+                        file_names.append(fn)
 
-                # Convert base64 response to image file
-                if res.status_code == 200:
-                    fn = "censored" + str(i) + ".jpg"
-                    img_bytes = res.json()["ImageBytes"].encode()
-                    with open(fn, "wb") as f:
-                        f.write(base64.decodebytes(img_bytes))
-                    i += 1
-                    file_names.append(fn)
+                # Upload images to Twitter and get their media ids
+                media_ids = []
+                for fn in file_names:
+                    res = self.api.media_upload(fn)
+                    media_ids.append(res.media_id)
 
-            # Upload images to Twitter and get their media ids
-            media_ids = []
-            for fn in file_names:
-                res = self.api.media_upload(fn)
-                media_ids.append(res.media_id)
-
-            # Tweet the results
-            try:
-                self.api.update_status(
-                    status="Successfully censored " + str(len(media_ids)) + " photos to protect anonymity.",
-                    in_reply_to_status_id=tweet.id,
-                    auto_populate_reply_metadata=True,
-                    media_ids=media_ids
-                )
-                logger.info("Success")
-            except Exception as e:
-                logger.error("Error on reply", exc_info=True)
+                # Tweet the results
+                try:
+                    self.api.update_status(
+                        status="Successfully censored " + str(len(media_ids)) + " photos to protect anonymity.",
+                        in_reply_to_status_id=tweet.id,
+                        auto_populate_reply_metadata=True,
+                        media_ids=media_ids
+                    )
+                    logger.info("Success")
+                except Exception as e:
+                    logger.error("Error on reply", exc_info=True)
+        else:
+            if is_reply is not None:
+                target_tweet = self.api.get_status(is_reply)
+                if target_tweet.user.screen_name == "photosense_bot":
+                    self.api.destroy_status(is_reply)
+                    logger.info("Successfully deleted tweet")
 
     def on_error(self, status):
         logger.error(status)
@@ -84,6 +96,32 @@ def mask_to_image(pixels):
             else:
                 arr[i, j] = [255, 255, 255]
     return Image.fromarray(arr)
+
+
+def get_tweet_flags(tweet):
+    # Get censor algorithm flag keys
+    arr = tweet.text.split()
+    if '@photosense_bot' in arr:
+        arr.remove('@photosense_bot')
+    for i in range(len(arr)):
+        arr[i] = arr[i].lower().replace("-", "")
+
+    # Create and return array of corresponding query params
+    flags = []
+    if 'px' in arr:
+        flags.append("pixel_sort")
+    if 'pz' in arr:
+        flags.append("pixelization")
+    if 'sb' in arr:
+        flags.append("gaussian_blur")
+    if 'bb' in arr:
+        flags.append("black_bar")
+    if 'fi' in arr:
+        flags.append("fill_in")
+    if 'rmv' in arr:
+        flags.append("remove")
+
+    return flags
 
 
 def main(keywords):
